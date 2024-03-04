f9ecb28303b48f2db90dc9b76bb36f28260a1054
Oscar Muñoz Moreno <oscar_novesano_mm@hotmail.com>
2024-03-04 23:34:15 +0100

    Add labels to graphs

--- lib/history.dart
+++ lib/history.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:temperature_app/data/models/record.dart';
 import 'package:temperature_app/data/providers/temperature_api.dart';
 
@@ -17,18 +18,18 @@ class HistoryPage extends StatefulWidget {
 
 class _HistoryPageState extends State<HistoryPage> {
   List<Record> _history = [];
-  
+
   @override
   void initState() {
     super.initState();
     _loadHistory();
   }
-  
+
   void _loadHistory() async {
     var data = await TemperatureApi().getHistory(
-        widget.id,
-        DateTime.now().subtract(const Duration(hours: 24)),
-        DateTime.now(),
+      widget.id,
+      DateTime.now().subtract(const Duration(hours: 24)),
+      DateTime.now(),
     );
     setState(() {
       _history = data;
@@ -54,18 +55,31 @@ class _HistoryPageState extends State<HistoryPage> {
                     child: LineChart(
                       LineChartData(
                         lineTouchData: const LineTouchData(),
-                        gridData: const FlGridData(),
-                        titlesData: const FlTitlesData(),
+                        gridData: const FlGridData(
+                          show: true,
+                        ),
+                        titlesData: const FlTitlesData(
+                          topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              reservedSize: 50,
+                              getTitlesWidget: timeTitleWidget,
+                            )
+                          ),
+                        ),
                         borderData: FlBorderData(),
                         lineBarsData: [
                           LineChartBarData(
-                              isCurved: false,
-                              dotData: const FlDotData(show: false),
-                              spots: _history.map((e) => FlSpot(e.time.toDouble(), e.temperature.toDouble())).toList()
+                            isCurved: false,
+                            dotData: const FlDotData(show: false),
+                            spots: _history.map((e) => FlSpot(e.time.toDouble(), e.temperature.toDouble())).toList()
                           ),
                         ],
-                        maxY: _history.isNotEmpty ? _history.map((e) => e.temperature).reduce(max).toDouble() + 5 : 0,
-                        minY: _history.isNotEmpty ? _history.map((e) => e.temperature).reduce(min).toDouble() - 5 : 0,
+                        maxY: _history.isNotEmpty ? _history.map((e) => e.temperature).reduce(max).roundToDouble() + 5 : 0,
+                        minY: _history.isNotEmpty ? _history.map((e) => e.temperature).reduce(min).roundToDouble() - 5 : 0,
                       ),
                       duration: const Duration(milliseconds: 250),
                     ),
@@ -80,19 +94,32 @@ class _HistoryPageState extends State<HistoryPage> {
                 children: [
                   const Text('Humedad', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
                   AspectRatio(
-                      aspectRatio: 2 / 1,
+                    aspectRatio: 2 / 1,
                     child: LineChart(
                       LineChartData(
                         lineTouchData: const LineTouchData(),
-                        gridData: const FlGridData(),
-                        titlesData: const FlTitlesData(),
+                        gridData: const FlGridData(
+                          show: true,
+                        ),
+                        titlesData: const FlTitlesData(
+                          topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              reservedSize: 50,
+                              getTitlesWidget: timeTitleWidget,
+                            )
+                          ),
+                        ),
                         borderData: FlBorderData(),
                         lineBarsData: [
                           LineChartBarData(
-                              color: Colors.orange,
-                              isCurved: false,
-                              dotData: const FlDotData(show: false),
-                              spots: _history.map((e) => FlSpot(e.time.toDouble(), e.humidity.toDouble())).toList()
+                            color: Colors.orange,
+                            isCurved: false,
+                            dotData: const FlDotData(show: false),
+                            spots: _history.map((e) => FlSpot(e.time.toDouble(), e.humidity.toDouble())).toList()
                           ),
                         ],
                         maxY: 100,
@@ -109,6 +136,16 @@ class _HistoryPageState extends State<HistoryPage> {
       ),
     );
   }
+}
+
+Widget timeTitleWidget(double value, TitleMeta meta) {
+  String fecha = DateFormat.Hm().format(DateTime.fromMillisecondsSinceEpoch(value.toInt()));
+
+  Widget text = RotationTransition(
+    turns: const AlwaysStoppedAnimation(45 / 360),
+    child: Text(fecha),
+  );
+  return SideTitleWidget(axisSide: meta.axisSide, space: 10, child: text);
 }
 
 List<Record> reduceRecords(List<Record> list) {
